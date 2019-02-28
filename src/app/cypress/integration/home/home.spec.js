17b19443186ca46da35a7c91da6424a051012bb7
ariel.duarte <arielpy@gmail.com>
2019-02-27 22:55:52 -0300

    updated home test

--- src/app/cypress/integration/home/home.spec.js
+++ src/app/cypress/integration/home/home.spec.js
@@ -6,6 +6,7 @@ describe('Tests para la pagina de Inicio (+ Nueva Inscripción)', () => {
             cy.get(data[0].elements.input_username).type(data[0].data.email);
             cy.get(data[0].elements.input_password).type(data[0].data.password);
             cy.get(data[0].elements.button_login).click();
+            cy.wait(1000)
             cy.get(data[0].elements.home_title).should('contain', data[0].asserts.home_title);
         })
     })
